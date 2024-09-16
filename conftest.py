a59d3bff357bcd30a2afe690682319914b50be89
Tom Close <tom.g.close@gmail.com>
2024-09-16 14:37:10 +1000

    typed conftest

--- conftest.py
+++ conftest.py
@@ -23,12 +23,13 @@ logger.addHandler(sch)
 # break at it
 if os.getenv("_PYTEST_RAISE", "0") != "0":
 
-    @pytest.hookimpl(tryfirst=True)  # type: ignore[misc]
-    def pytest_exception_interact(call: ty.Any) -> None:
-        raise call.excinfo.value
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_exception_interact(call: pytest.CallInfo[ty.Any]) -> None:
+        if call.excinfo is not None:
+            raise call.excinfo.value
 
-    @pytest.hookimpl(tryfirst=True)  # type: ignore[misc]
-    def pytest_internalerror(excinfo: ty.Any) -> None:
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_internalerror(excinfo: pytest.ExceptionInfo[BaseException]) -> None:
         raise excinfo.value
 
 
